c7b9320bf37c09c40224d241110c4083ad901f12
rcaceres <me@rchrd.net>
2014-04-03 13:57:13 -0700

    Restructure to work with pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pytzconvert',
+    version='0.1',
+    packages=['pytzconvert',],
+    description= "Convert non-pytz timezones to pytz timezones.",
+    long_description=open('README.md').read(),
+    url="https://github.com/rchrd2/pytzconvert",
+    author="Richard Caceres",
+    author_email="me@rchrd.net",
+    install_requires=['pytz']
+)
